f19ee7fa1d1ca0907dbe9aca99704fe0c870a8c8
luoyin <luoyin1994@icloud.com>
2017-03-14 12:09:16 +0800

    0.0.8

---
+++ dev/script/others/compress.js
@@ -0,0 +1,23 @@
+/** 引入模块 **/
+const path = require('path')
+const packcompressto = require('packcompressto')
+
+/** 定义路径 **/
+let root                = path.resolve(__dirname, '../../../')
+let path_public         = root + '/public'
+let path_font_index     = path_public + '/index/font-icon'
+let path_font_ajaxIndex = path_public + '/ajaxIndex/font-icon'
+let path_font_book      = path_public + '/book/font-icon'
+let path_font_ajaxBook  = path_public + '/ajaxBook/font-icon'
+let path_font_reader    = path_public + '/reader/font-icon'
+
+/** 压缩打包 index font-icon/style.css **/
+packcompressto.packTo_font(path_font_index, path_font_index)
+/** 压缩打包 ajaxIndex font-icon/style.css **/
+packcompressto.packTo_font(path_font_ajaxIndex, path_font_ajaxIndex)
+/** 压缩打包 book font-icon/style.css **/
+packcompressto.packTo_font(path_font_book, path_font_book)
+/** 压缩打包 ajaxBook font-icon/style.css **/
+packcompressto.packTo_font(path_font_ajaxBook, path_font_ajaxBook)
+/** 压缩打包 reader font-icon/style.css **/
+packcompressto.packTo_font(path_font_reader, path_font_reader)
